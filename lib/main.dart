1951175f79f8d7a1d6107f2e5b86b720f0ca329c
Blaireshon <sonsy09@naver.com>
2024-04-03 17:00:32 +0900

    자식들까지 위젯 반환 완료. positioned 진행 중

--- lib/main.dart
+++ lib/main.dart
@@ -32,68 +32,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late Future<Widget> flutterView;
+  //late Future<Widget> flutterView;
 
-  @override
-  void initState(){
-    super.initState();
-    // getData('JsmdZX9BH4IxJ0YZqnww2q','0-1');
-    flutterView = FigmaToFlutter.getData(fileKey: 'JsmdZX9BH4IxJ0YZqnww2q',id: '0-1') as Future<Widget>;
-  }
+  // @override
+  // void initState(){
+  //   super.initState();
+  //   // getData('JsmdZX9BH4IxJ0YZqnww2q','0-1');
+  //   flutterView = FigmaToFlutter.getData(fileKey: 'JsmdZX9BH4IxJ0YZqnww2q',id: '1-2');
+  // }
 
   @override
   Widget build(BuildContext context) {
+    final Future<Widget> flutterView = FigmaToFlutter.getData(fileKey: 'JsmdZX9BH4IxJ0YZqnww2q',id: '1-2');
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 200,
-              child: ElevatedButton(
-                onPressed: () {
-                  // Call getData and use its return value in the UI
+      body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              //flutterView
+              FutureBuilder(
+                future: flutterView,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    // While waiting for data, return a CircularProgressIndicator or a placeholder
+                    return CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    // If there's an error, display an error message
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    // If data is successfully fetched, return the data widget
+                    //return snapshot.data ?? Container(); // Make sure to handle null case
+                    return  snapshot.data ?? Container();
+                  }
                 },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xffFCAF17),
-                  elevation: 0,
-                ),
-                child: const Text('top position',
-                    style: TextStyle(color: Colors.white)),
               ),
-            ),
-            SizedBox(height: 10),
-            // Use FutureBuilder to handle the asynchronous operation
-
-            FutureBuilder(
-              future: flutterView,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  // While waiting for data, return a CircularProgressIndicator or a placeholder
-                  return CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  // If there's an error, display an error message
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  // If data is successfully fetched, return the data widget
-                  return snapshot.data ?? Container(); // Make sure to handle null case
-                }
-              },
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
+
     );
   }
-
-  // void getData(String fileKey, String id){
-  //   FigmaToFlutter.getData(fileKey: fileKey, id: id);
-  // }
-  // FigmaToFlutter getData(String fileKey, String id){
-  //   return FigmaToFlutter.getData(fileKey: fileKey, id: id);
-  // }
 }
